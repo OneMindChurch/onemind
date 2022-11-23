78614c4e063c4737dbfbc5787a98944f93c4a7ef
jeongwoo <mbh219@naver.com>
2022-11-23 16:07:36 +0900

    Redux 설치

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -5,31 +5,12 @@ import Home from './components/Home'
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          시공의 폭풍은 정말 놀라워~!
-        </a>
-      </header>
-    </div>
-=======
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route element={<NotFound />} />
       </Routes>
     </BrowserRouter>
->>>>>>> upstream/feature
   );
 }
 
