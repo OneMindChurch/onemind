11dd0117bd2bf1a8d1369609abfcd29aa859d4e4
jeongwoo <mbh219@naver.com>
2022-11-23 16:30:13 +0900

    React mUI

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NotFound from './components/NotFound'
 import Home from './components/Home'
+import NavBar from './components/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
+    <NavBar></NavBar>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route element={<NotFound />} />
---
+++ front/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import "./CSS/NavBar.css"
+
+const NavBar = () => {
+    return(
+        <>
+        <div class="wrap">
+            <div class="first"></div>
+            <div class="second"></div>
+        </div>
+        </>
+    )
+}
+export default NavBar;
