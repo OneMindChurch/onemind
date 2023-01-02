59188bd30da8b10145740d834ae13d2f93c1a8b8
Deng Lee <edaero22@naver.com>
2023-01-02 19:19:39 +0900

    Docs: styled component arrangement

--- front/src/styles/Intro.tsx
+++ front/src/styles/Intro.tsx
@@ -4,7 +4,7 @@ import { theme } from "./Theme";
 const Flex = styled.div`
   display: flex;
 `;
-// Introduction & Location 공통
+// Component 공통
 export const Wrap = styled(Flex)`
   width: 100%;
   padding: 0 0 25px 0;
@@ -23,22 +23,15 @@ export const Img = styled.img<{ width: any; height: any; margin: any }>`
   margin: ${(props) => props.margin};
   object-fit: cover;
 `;
-export const FloatImg = styled.img<{ float: any }>`
-  width: 30%;
-  float: ${(props) => props.float};
-  margin-left: 20px;
 
-  @media ${(props) => props.theme.mobile} {
-    display: none;
-  }
-`;
 export const DivideBox = styled(Flex)`
   width: 100%;
   max-width: 1280px;
   flex-flow: row wrap;
   gap: 25px;
 `;
-export const SidebarBox = styled(Flex)`
+export const SidebarBox = styled.div`
+  display: flex;
   position: -webkit-sticky;
   position: sticky;
   top: 50px;
@@ -62,7 +55,6 @@ export const Ul = styled.ul`
 export const Li = styled.li`
   list-style: none;
 `;
-
 export const ContentsBox = styled(Flex)`
   flex-flow: column wrap;
   gap: 30px;
@@ -72,7 +64,17 @@ export const ContentsBox = styled(Flex)`
 `;
 export const ContentsDiv = styled.div`
   width: 100%;
-  max-height: 1000px;
+`;
+export const TinyTitle = styled.p<{ fontsize: any }>`
+  font-size: ${(props) => props.fontsize};
+  font-weight: bold;
+  text-align: center;
+`;
+export const TinyTitles = styled.p<{ fontsize: any }>`
+  font-size: ${(props) => props.fontsize};
+  font-weight: bold;
+  text-align: start;
+  margin-left: 60px;
 `;
 export const InfoDiv = styled.div`
   width: 100%;
@@ -86,57 +88,6 @@ export const InfoDiv = styled.div`
     gap: 20px;
   }
 `;
-
-export const GreetingInfoDiv = styled.div`
-  width: 100%;
-  height: 100%;
-  text-align: left;
-  position: relative;
-`;
-export const TinyTitle = styled.p<{ fontsize: any }>`
-  font-size: ${(props) => props.fontsize};
-  font-weight: bold;
-  text-align: center;
-`;
-export const TinyTitles = styled.p<{ fontsize: any }>`
-  font-size: ${(props) => props.fontsize};
-  font-weight: bold;
-  text-align: start;
-  margin-left: 60px;
-`;
-export const Strong = styled.strong`
-  font-color: ${theme.mainColor};
-`;
-export const IntroP1 = styled.div`
-  font-size: 1.7vh;
-  width: 70%;
-  padding: 0 30px 0 12px;
-  margin: 0 0 50px 30px;
-
-  @media ${(props) => props.theme.desktop} {
-    font-size: 1.4vw;
-  }
-  @media ${(props) => props.theme.mobile} {
-    font-size: 0.5vh;
-  }
-`;
-export const IntroP2 = styled.p`
-  padding: 0 0 0 70px;
-  margin-bottom: 50px;
-  @media ${(props) => props.theme.mobile} {
-    padding: 10px;
-    margin-bottom: 20px;
-  }
-`;
-// Intro
-export const MainIntroBox = styled.div`
-  position: relative;
-`;
-export const MainIntroStart = styled.div`
-  top: 0;
-  position: absolute;
-  padding: 0 30px 0 20px;
-`;
 export const InfoTitleDiv = styled(InfoDiv)<{ fontsize: any }>`
   height: 40px;
   text-align: left;
@@ -144,190 +95,7 @@ export const InfoTitleDiv = styled(InfoDiv)<{ fontsize: any }>`
   font-weight: bold;
   border-bottom: 1px solid lightgrey;
 `;
-export const IntroFirstTitleBox = styled.div`
-  width: 100%;
-  display: flex;
-  flex-flow: row wrap;
-  gap: 90px;
-`;
-export const IntroTitleBox = styled.div`
-  width: 100%;
-  display: flex;
-  flex-flow: row wrap;
-  align-items: center;
-  gap: 10px;
-  position: relative;
-`;
-export const IntroImg = styled.div`
-  display: flex;
-  align-items: center;
-  height: 45px;
-  margin: 10px 0 20px 0;
-`;
-export const IntroFirstTitle = styled.div<{ fontsize: any }>`
-  display: flex;
-  align-items: center;
-  flex: 8;
-  height: 45px;
-  font-size: ${(props) => props.fontsize};
-  font-weight: bold;
-  text-align: start;
-  margin: 30px 0 30px 120px;
-  padding-left: 5px;
-  border-radius: 5px;
-
-  @media ${(props) => props.theme.desktop} {
-    margin: 30px 0 10px 120px;
-  }
-  @media ${(props) => props.theme.mobile} {
-    margin: 45px 0 0 5px;
-    font-size: 40px;
-  }
-`;
-export const IntroTitle = styled.div<{ fontsize: any }>`
-  display: flex;
-  align-items: center;
-  flex: 8;
-  height: 45px;
-  font-size: ${(props) => props.fontsize};
-  font-weight: bold;
-  text-align: start;
-  margin: 10px 0 20px 0;
-  padding-left: 5px;
-  border: 2px solid ${theme.mainColor};
-  border-radius: 5px;
-`;
-export const GreetingP1 = styled.div<{ fontsize: any }>`
-  color: ${theme.mainColor};
-  font-size: ${(props) => props.fontsize};
-  font-weight: bold;
-  width: 100%;
-
-  @media ${(props) => props.theme.desktop} {
-    font-size: 1.8vw;
-  }
-  @media ${(props) => props.theme.mobile} {
-    font-size: 2.5vw;
-  }
-`;
-export const GreetingP2 = styled.div<{ fontsize: any }>`
-  width: 100%;
-  font-size: ${(props) => props.fontsize};
-
-  @media ${(props) => props.theme.desktop} {
-    font-size: 1.3vw;
-  }
-  @media ${(props) => props.theme.mobile} {
-    font-size: 2.1vw;
-  }
-`;
-
-export const IntroNum = styled.div`
-  top: 12px;
-  left: 15px;
-  font-size: 32px;
-  font-weight: bold;
-  position: absolute;
-`;
-// Location 적용
-export const MapBox = styled.div`
-  width: 100%;
-  height: 500px;
-  border: 1px solid ${theme.mainColor};
-`;
-// Sidebar 적용
-export const H2 = styled.h2`
-  height: 45px;
-  border-bottom: 1px solid lightgrey;
-  text-align: center;
-`;
-export const Lihover = styled(Li)`
-  width: 100%;
-  height: 32px;
-  color: black;
-  text-decoration: none;
-  font-weight: bold;
-  border: 2px solid ${theme.mainColor};
-  border-radius: 8px;
-  text-align: center;
-  padding: 10px 0 6px 0;
-  &:hover {
-    font-size: 18px;
-    background-color: ${theme.mainColor};
-    color: ${theme.subColor};
-  }
-`;
-// Worship 적용
-export const WorshipItem = styled(Flex)`
-  border-left: 2px solid ${theme.mainColor};
-  border-right: 2px solid ${theme.mainColor};
-  border-radius: 20px;
-  width: 100%;
-  height: 300px;
-  margin: 20px 0 0 0;
-  justify-content: space-between;
-  align-items: center;
-  flex-flow: row wrap;
-`;
-export const WorshipInnerItem = styled(Flex)`
-  width: 33%;
-  flex-flow: column wrap;
-  justify-content: space-between;
-  align-items: center;
-  gap: 20px;
-
-  @media ${(props) => props.theme.mobile} {
-    width: 50%;
-  }
-`;
 export const P = styled.p`
   // border-bottom: 1px double ${theme.mainColor};
   border-bottom: 2px double lightgrey;
-`;
-// Pastor 적용
-export const PastorDiv = styled(Flex)`
-  width: 100%;
-  justify-content: start;
-  align-items: start;
-  flex-flow: row wrap;
-  margin-bottom: 50px;
-  gap: 20px;
-`;
-export const PastorItem = styled(Flex)`
-  flex-flow: column wrap;
-  justify-content: space-around;
-  align-items: center;
-  border: 1px solid lightgrey;
-  border-radius: 5px;
-  padding: 15px;
-  margin: 15px;
-  gap: 5px;
-`;
-// Contribution 적용
-export const ContributionItem = styled(Flex)`
-  border-left: 2px solid ${theme.mainColor};
-  border-right: 2px solid ${theme.mainColor};
-  border-radius: 20px;
-  width: 100%;
-  height: 100%;
-  margin: 20px 0 0 0;
-  justify-content: center;
-  align-items: center;
-  flex-flow: row wrap;
-
-  @media ${(props) => props.theme.mobile} {
-    flex-flow: column wrap;
-    gap: 50px;
-  }
-`;
-export const ContributionInnerItem = styled(Flex)`
-  width: 45%;
-  flex-flow: column wrap;
-  justify-content: space-between;
-  align-items: center;
-  gap: 20px;
-`;
-// Greeting
-export const GreetingAbsolute = styled.div`
-  position: absolute;
 `;
